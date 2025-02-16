7c036c4b4872eed56d030cbd7b15748b44c5d9f9
Ale M <bomberstudios@gmail.com>
2025-02-16 22:51:16 +0100

    Fixes to feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,9 +1,11 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts'
 
-export async function GET(context: { site: any; }) {
-  const posts = await getCollection('blog');
+export async function GET(context: { site: any }) {
+  const posts = (await getCollection('blog')).sort((a, b) => {
+    return new Date(b.data.date).getTime() - new Date(a.data.date).getTime()
+  })
 
   return rss({
     title: SITE_TITLE,
@@ -14,7 +16,7 @@ export async function GET(context: { site: any; }) {
       date: post.data.date,
       description: post.data.description,
       content: post.rendered?.html,
-      link: `/${post.id}/`,
-    })),
-  });
+      link: `/${post.id}/`
+    }))
+  })
 }
